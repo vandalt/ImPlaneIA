ab7011c638c9fdd2658b3a8d21ac833edec974de
anand0xff <anand0xff@gmail.com>
2019-11-05 16:20:24 -0500

    amisim2mirage fixed?

--- notebooks/amisim2mirage.py
+++ notebooks/amisim2mirage.py
@@ -17,28 +17,29 @@ mirexample = "jw00793001001_01101_00001_nis_cal.fits"
 
 for fname in (tr, cr):
     fobj_ami_sim = fits.open(datadir+fname+".fits")
+
+    print(fobj_ami_sim[0].data.shape)
+    d = fobj_ami_sim[0].data
+    print(d.shape, type(d))
+    samples = d[:, 0, :]
+    print(samples.shape, samples.mean())
+
+
     mirobj = fits.open(datadir+mirexample) # read in sample mirage file
     print(mirobj[1].data.shape)
     mirobj[1].data[:,:] = 0.0 # zero out mirage scihdr data
     print(mirobj[1].data.shape)
     
     # make cube of data for mirage from input ami_sim file...
-    data = np.zeros((fobj_ami_sim[0].data.shape[0], #"slices of data cube of integrations"
-                           mirobj[1].data.shape[0], 
-                           mirobj[1].data.shape[1]))
-    data[:,:77,:77] = fobj_ami_sim[0].data[:,:,:] # replace with ami_sim data
-    print(data.shape)
-    print(fobj_ami_sim[0].data.shape)
-    d = fobj_ami_sim[0].data
-    print(d.shape, type(d))
+    mirobj[1].data = np.zeros((fobj_ami_sim[0].data.shape[0], #"slices of data cube of integrations"
+                                     mirobj[1].data.shape[0], 
+                                     mirobj[1].data.shape[1]))
+    mirobj[1].data[:,:,:] = samples.mean()
+    mirobj[1].data = mirobj[1].data.astype(np.float32)
+
+    mirobj[1].data[:,:77,:77] = fobj_ami_sim[0].data[:,:,:] # replace with ami_sim data
 
     import pdb; pdb.set_trace()
-    samples = d[:, 0, :]
-    print(samples.shape, samples.mean())
-
-    #import pdb; pdb.set_trace()
-    mirobj[1].data[:,77:,:] = samples.mean()
-    mirobj[1].data[:,:,77:] = samples.mean()
 
     mirobj.writeto(datadir+fname+mirext+".fits", overwrite=True)
     
