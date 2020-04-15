74b581cde9c96780fab8d7681b1c0b9acf1d301c
Deepashri Thatte <thatte@stsci.edu>
2020-04-15 18:09:53 -0400

    Updated driver to take into account psf offset for even fov, to use zero pistons as default, formatted screen ouput, added CP mean and sigma to the output.

--- notebooks/implaneia_driver.py
+++ notebooks/implaneia_driver.py
@@ -34,17 +34,18 @@ mirexample = os.path.expanduser('~') + \
              "/ImplaneIA/notebooks/simulatedpsfs/" + \
              "jw00793001001_01101_00001_nis_cal.fits"
 
+fov = 80
 filt="F430M"
 oversample=11
 bandpass = np.array([(1.0, 4.3e-6),])
 
-#datafiles = ("""all_effects_data_mir.fits all_effects_data_mir_copy.fits""").split()
-datafiles = (fitsimdir+'all_effects_data_mir.fits',)# all_effects_data_mir_copy.fits""").split()
 
+datafiles = (fitsimdir+'all_effects_data_mir.fits',)# all_effects_data_mir_copy.fits""").split()
+np.random.seed(100)
 #datafiles=['all_effects_data_mir.fits','all_effects_data_mir_copy.fits']
 
 
-def simulate_data(rot_deg=0.0,psf_offsets_det = (0.0,0.0),phi_waves = None):
+def simulate_data(rot_deg=0.0,psf_offsets_det = (0.0,0.0),phi_waves = np.zeros((7,))):
 
     pixelscale_as=0.0656
     arcsec2rad = u.arcsec.to(u.rad)
@@ -62,9 +63,13 @@ def simulate_data(rot_deg=0.0,psf_offsets_det = (0.0,0.0),phi_waves = None):
     #*************ADD OFFSETS TO PERFECT DATA************************************************
 
     #Use psf_offset from the function call
+    if (fov%2) == 0:
+       _psf_offsets_det = (0.5+psf_offsets_det[0],0.5+psf_offsets_det[1])
+    else:
+       _psf_offsets_det = (0.0+psf_offsets_det[0],0.0+psf_offsets_det[1])
 
     #************ADD PISTONS TO PERFECT DATA************************************************
-    np.random.seed(100)
+    #np.random.seed(100)
 
     #lambda/14 ~ 80% strehl ratio
     #phi = np.random.normal(0,1.0, 7) / 14.0 # waves
@@ -75,11 +80,13 @@ def simulate_data(rot_deg=0.0,psf_offsets_det = (0.0,0.0),phi_waves = None):
     print("phi_nb stdev/w", phi.std())
     print("phi_nb stdev/r", phi.std()*2*np.pi)
     print("phi_nb mean/r", phi.mean()*2*np.pi)
-    pistons = 0.0*phi *4.3e-6 #zero or non-zero pistons
-    #pistons = phi *4.3e-6   #meters
 
-    print("/=====input pistons/m=======/\n",pistons)
-    print("/=====input pistons/r=======/\n",pistons*(2*np.pi)/4.3e-6)
+    pistons = phi *4.3e-6   #meters
+    #Temporarily overwrite with smaller pistons measured from CV3 data
+    #pistons = np.array([5.25e-13, -2.054e-13, -4.300e-14, -2.38e-13,6.21e-15, -2.036e-13,  1.60e-13])
+
+    #print("/=====input pistons/m=======/\n",pistons)
+    #print("/=====input pistons/r=======/\n",pistons*(2*np.pi)/4.3e-6)
 
     
 
@@ -87,12 +94,13 @@ def simulate_data(rot_deg=0.0,psf_offsets_det = (0.0,0.0),phi_waves = None):
     jw = NRM_Model(mask='jwst', holeshape="hex", affine2d=aff)
     jw.set_pixelscale(pixelscale_as*arcsec2rad)
     jw.set_pistons(pistons)
-    jw.simulate(fov=35, bandpass=bandpass, over=oversample,psf_offset=psf_offsets_det )
+    jw.simulate(fov=fov, bandpass=bandpass, over=oversample,psf_offset=_psf_offsets_det )
     fits.PrimaryHDU(data=jw.psf).writeto(fitsimdir+"all_effects_data.fits",overwrite=True)
 
-    #**********Convert simulated data to mirage format***************************************
+    #**********Convert simulated data to mirage format.***************************************
 
     amisim2mirage( fitsimdir, ("all_effects_data",), mirexample, filt)
+    return aff, psf_offsets_det, pistons,
 
     
 def analyze_data(test_tar, affine2d = None, set_center=(0.0,0.0), rotsearch_d = None, set_pistons = None):
@@ -104,21 +112,23 @@ def analyze_data(test_tar, affine2d = None, set_center=(0.0,0.0), rotsearch_d = 
     print(test_tar)
     
     data = fits.getdata(test_tar)
-    print(data.shape)
-    
+    print(data.shape[1])
+    dim = data.shape[1]
+
+    if (dim%2) == 0:
+        set_center = (0.5+set_center[0],0.5+set_center[1])
+    else:
+        set_center = (0.0+set_center[0],0.0+set_center[1])
     
     from nrm_analysis import find_affine2d_parameters as FAP
     mx, my, sx,sy, xo,yo, = (1.0,1.0, 0.0,0.0, 0.0,0.0)
-    #rotsearch_d = (8.0, 8.5, 9.0, 9.5, 10.0, 10.5, 11.0, 11.5, 12.0)
-    #rotsearch_d = (-1.0, 0.0,1.0,2.0,3.0,4.0, 5.0)
-    #rotsearch_d = (-3.0, -2.0,-1.0,0.0,1.0,2.0, 3.0)
     
     psf_offset = set_center
 
     if rotsearch_d: 
         affine2d = FAP.find_rotation(data[0,:,:], psf_offset,
                       rotsearch_d, mx, my, sx, sy, xo,yo,
-                      PIXELSCALE_r, 35, bandpass, oversample, holeshape, outdir=fitsimdir)
+                      PIXELSCALE_r, dim, bandpass, oversample, holeshape, outdir=fitsimdir)
     
         print("***********measured aff: aff_rot_measured**************")
         affine2d.show()
@@ -130,31 +140,38 @@ def analyze_data(test_tar, affine2d = None, set_center=(0.0,0.0), rotsearch_d = 
     print(test_tar)
     ff_t.fit_fringes(test_tar)
 
+    data=fits.getdata(test_tar)
+    print("***** Mean CP ",ff_t.nrm.redundant_cps.mean(), " *****")
+    print("***** Sigma CP ",ff_t.nrm.redundant_cps.std(), " *****")
+
     pos = np.get_printoptions()
     np.set_printoptions(precision=3, formatter={'float': lambda x: '{:+.3e}'.format(x)},
                             linewidth=80)
+    print("***** n_residual[36:-36,36:-36] *****")
     print((ff_t.nrm.residual/data.max())[36:-36,36:-36])
-    print("*****  measured pistons ******/r", ff_t.nrm.fringepistons)
-    print("output pistons/r",ff_t.nrm.fringepistons)
-    print("output pistons/w",ff_t.nrm.fringepistons/(2*np.pi))
-    print("output pistons/m",ff_t.nrm.fringepistons*4.3e-6/(2*np.pi))
-    #print("input pistons/m ",jw.phi)
+    ##print("*****  measured pistons ******/r", ff_t.nrm.fringepistons)
+    ##print("output pistons/r",ff_t.nrm.fringepistons)
+    ##print("output pistons/w",ff_t.nrm.fringepistons/(2*np.pi))
+    ##print("output pistons/m",ff_t.nrm.fringepistons*4.3e-6/(2*np.pi))
     return affine2d, (ff_t.nrm.xpos, ff_t.nrm.ypos),  ff_t.nrm.fringepistons,
     print("done")
-    ##np.set_printoptions(pos)
+    np.set_printoptions(pos)
     
     
 
 if __name__ == "__main__":
 
-    rot = 0.0
+    rot = 2.0
     rot = avoidhexsingularity(rot) # in utils
     aff = Affine2d(rotradccw=np.pi*rot/180.0, name="{0:.0f}".format(rot)) # in utils
     _rotsearch_d = (-3.0, -2.0,-1.0,0.0,1.0,2.0, 3.0)
-   
+
+    #affine2d_in,psf_offsets_in,pistons_in = simulate_data(rot_deg=rot,psf_offsets_det= (0.0,0.0), phi_waves=np.random.normal(0,1.0, 7)/14.0)
+    affine2d_in,psf_offsets_in,pistons_in = simulate_data(rot_deg=rot,psf_offsets_det= (0.48,0.0), phi_waves=np.random.normal(0,1.0, 7)/14.0)
     
     for df in datafiles:
         print("analyzing", df)
+        datafile = fits.getdata(df)
 
         """
         Find rotation and use for analysis
@@ -162,33 +179,22 @@ if __name__ == "__main__":
         OR
         affine object (aff) created using user supplied rotation
         analyze_data(df,affine2d = aff, set_center=(0.0,0.0), rotsearch_d = None, set_pistons = None)
+        OR
+        affine object (aff) created using user supplied rotation and user supplied psf_offset
+        analyze_data(df,affine2d = aff, set_center=(0.48,0.0), rotsearch_d = None, set_pistons = None)
         """
 
         _aff, _psf_offset, _pistons = analyze_data(df,affine2d = None, set_center=(0.0,0.0), rotsearch_d = _rotsearch_d, set_pistons = None)
-        #_aff, _psf_offset, _pistons = analyze_data(file,affine2d = aff, set_center=(0.48,0.0), rotsearch_d = None, set_pistons = None)
+        #_aff, _psf_offset, _pistons = analyze_data(df,affine2d = aff, set_center=(0.48,0.0), rotsearch_d = None, set_pistons = None)
         
         print("/====measured affine=========/")
         _aff.show()
         print("/=====measured psf_offset====/\n",_psf_offset)
-        print("/=====output pistons/r=======/\n",_pistons)
         
         
-        """
-        Work in progress
-        aff_list =[ ]
-        i=0
-        while(i<= 4):
-           
-           _aff, _psf_offset, _pistons = analyze_data(file,affine2d = aff, set_center=(0.48,0.0), rotsearch_d = None, set_pistons = None)
-           aff_list.append(_aff)
-           print(type(_aff))
-           affine2d = _aff
-           set_center = _psf_offset
-           print(_psf_offset, _pistons), 
-           _aff.show()  #ERROR  AttributeError: 'tuple' object has no attribute 'show'
-           i = i+1 
-        """
-         
+    print("/=====input affine==========/")
+    affine2d_in.show()
+    print("/=====input psf_offset=======/\n", psf_offsets_in)
+    #print("/=====input pistons/r=======/\n",pistons_in*2*np.pi/4.3e-6)
+    utils.compare_pistons(pistons_in*2*np.pi/4.3e-6,_pistons)
     print("Examine the residuals in", fitsimdir, "\n")
-        
-       
