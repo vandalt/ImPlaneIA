2086d5c3d9702dc11c1ec53746839c11ebf13111
anand0xff <anand0xff@gmail.com>
2020-05-11 13:38:28 -0400

    improved documentation

--- scripts/read_oitxt.py
+++ scripts/read_oitxt.py
@@ -1,19 +1,23 @@
 #! /usr/bin/env python
 """
 Reads ImPlaneIA output text of [CPs,phases,amplitudes]_nn.txt in one directory
+    OI Observables are: np arrays (nslices,nobservables) in shape.
+        self.fp = np.zeros((self.nslices, self.nbl))
+        self.fa = np.zeros((self.nslices, self.nbl))
+        self.cp = np.zeros((self.nslices, self.ncp))
+        if len(self.observables) == 4:
+            self.ca = np.zeros((self.nslices, self.nca))
 """
 import os
 import numpy as np
 
 from nrm_analysis.misctools.implane2oifits import ObservablesFromText 
 
-oitxtpath =  os.path.expanduser('~')+"/gitsrc/ImPlaneIA/example_data/example_niriss/bin_cal_oitxt" #implaneia text oi observables in here 
 
 # OI observables from text for 7-hole mask...
-oiobs = ObservablesFromText(7, oitxtpath, verbose=True)
+# nholes in first argument.  
+# implaneia text oi observables in the second argument, the 'oitxtpath'
+oiobs = ObservablesFromText(7, 
+                            os.path.expanduser('~')+"/gitsrc/ImPlaneIA/example_data/example_niriss/bin_cal_oitxt" ,
+                            verbose=True)
 oiobs.showdata()
-
-
-
-
-
