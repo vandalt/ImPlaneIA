540af5a0eb83259c9c0254338d344715ba97a668
anand0xff <anand0xff@gmail.com>
2020-07-13 17:40:38 -0400

    calibration driver operating on avgd oifits from observations

---
+++ scripts/driver_abdor_calibrate.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import os
+import numpy as np
+from astropy.io import fits
+from astropy import units as u
+import sys
+import glob
+import string
+import nrm_analysis
+from nrm_analysis.misctools import oifits  
+
+def main(alloifsdir):
+    print('Searching',alloifsdir, 'for *.oifits files')
+
+    filters = ['F480M', 'F430M', 'F380M']
+    obslist = ['Obs_1_00','Obs_2_00','Obs_4_00', 'Obs_6_00']
+    targlist = ['ABDOR', 'HD36805', 'HD37093']
+
+    oifinputdict = {}
+    oifs = glob.glob(alloifsdir+'*.oifits')
+    for oif in oifs:
+        oifinputdict[(oif.split('.')[-2]).split('/')[-1]] = oifits.load(oif)
+
+    print('oifits files found are read into a dict of oifits.load() dic structures')
+    for clef in (oifinputdict.keys()):
+        print('  ', '{:55s}'.format(clef), 
+              '{:10s}'.format(oifinputdict[clef]['info']['TARGET']),
+              '{:10s}'.format(oifinputdict[clef]['info']['FILT']), 
+              )
+
+if __name__ == "__main__":
+               
+    main(os.path.expanduser('~') +'/data/implaneia/amisim_udem_nis019/collected_oifits/scriptdev/')
